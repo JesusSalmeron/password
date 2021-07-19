eed2d838da7ac635597171d4bf95c77793c2dfc2
JesusSalmeron <79457534+JesusSalmeron@users.noreply.github.com>
2021-07-19 10:59:58 +0200

    Add files via upload

---
+++ generador.py
@@ -0,0 +1,19 @@
+import random
+from werkzeug.security import generate_password_hash
+
+minus = "abcdefghijklmnopqrstuvvxyz"
+mayus = minus.upper()
+numeros="1234567890"
+simbolos="!@·$%&/()=?¿"
+
+base = minus + mayus + numeros + simbolos
+
+longitud = 12
+
+for _ in range(10):
+    muestra = random.sample(base,longitud)
+    password = "".join(muestra)
+    password_encriptado = generate_password_hash(password)
+    print("{} => {}".format(password, password_encriptado))
+
+
